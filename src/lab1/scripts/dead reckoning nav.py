c3be52d3197a07c556643b274813009b2d4c4242
Dvckhv <patricio.hinostrozav@uc.cl>
2023-04-11 20:25:08 -0400

    Movement demo

---
+++ src/lab1/scripts/dead reckoning nav.py
@@ -0,0 +1,38 @@
+import rospy as rp
+from geometry_msgs.msg import Twist
+import time
+import math
+#importar topicos
+
+
+class Movement(object):
+    def __init__(self) -> None:
+        self.v = 0.2 # m/s
+        self.ang = 1 #rads/s
+        rp.init_node("Movement_manager")
+        self.vel_applier = rp.Publisher("yocs_cmd_vel_mux/input/navigation",
+                Twist, queue_size = 10)
+        self.rate_hz = 10
+        self.rate = rp.Rate(self.rate_hz)
+        self.init_pose = (0,0,0)
+
+
+    def aplicar_velocidad(self,speed_command: list ):
+        for mov in speed_command:
+            inicial_time = time.time()
+            while time.time()<= inicial_time+mov[2]:
+                speed= Twist()
+                speed.linear.x, speed.angular.z= mov[0],mov[1]            
+                rp.loginfo("linear speed:", mov[0],"angular speed:",mov[1])
+                self.vel_applier.publish(speed)
+                self.rate.sleep()
+
+    def mover_robot_a_destino(self, goal_pose):
+        x_movement, y_movement, t_movement = goal_pose[0], goal_pose[1], goal_pose[2]
+        
+        intructions = [( self.v, 0, x_movement / self.v ), ( 0, self.ang, math.pi / self.ang ),
+                        ( self.v, 0, y_movement / self.v ), ( 0, self.ang, t_movement / self.v )]
+        self.aplicar_velocidad(intructions)
+
+        
+    def accion_mover(self,)
