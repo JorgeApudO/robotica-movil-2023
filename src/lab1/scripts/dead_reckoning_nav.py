db4dd1c1ce5aec0f83692d0d1e38800604b88271
Dvckhv <patricio.hinostrozav@uc.cl>
2023-04-16 18:26:55 -0400

    functional movement

--- src/lab1/scripts/dead_reckoning_nav.py
+++ src/lab1/scripts/dead_reckoning_nav.py
@@ -6,6 +6,7 @@ from tf.transformations import euler_from_quaternion
 import time
 import math
 
+
 class Movement(object):
     def __init__(self) -> None:
         self.v = 0.2  # m/s
@@ -14,7 +15,8 @@ class Movement(object):
         self.vel_applier = rp.Publisher("yocs_cmd_vel_mux/input/navigation", Twist, queue_size=10)
         self.rate_hz = 10
         self.rate = rp.Rate(self.rate_hz)
-        self.mov_sub = rp.Subscriber("goal_list", PoseArray, self.accion_mover)  # investigar PoseArray
+        self.mov_sub = rp.Subscriber("goal_list", PoseArray,
+                                     self.accion_mover)  # investigar PoseArray
         self.current_pose = Pose()
 
     def aplicar_velocidad(self, speed_command: list):
@@ -23,40 +25,57 @@ class Movement(object):
             while time.time() <= inicial_time+mov[2]:
                 speed = Twist()
                 speed.linear.x, speed.angular.z = mov[0], mov[1]
-                rp.loginfo("linear speed:", mov[0], "angular speed:", mov[1])
                 self.vel_applier.publish(speed)
                 self.rate.sleep()
 
     def mover_robot_a_destino(self, goal_pose: Pose):  # EDITAR POSEARRAY
-        x_goal, y_goal = goal_pose.position.x(), goal_pose.position.y()
-        yaw_goal = euler_from_quaternion(*goal_pose.orientation())[2]
+        x_goal, y_goal = goal_pose.position.x, goal_pose.position.y
+        yaw_goal = euler_from_quaternion((goal_pose.orientation.x, goal_pose.orientation.y,
+                                         goal_pose.orientation.z, goal_pose.orientation.w))[2]
         instructions = []  # Hay que definir esto bien
-        x_initial,y_initial = self.current_pose.position.x(), self.current_pose.position.y()
-        yaw_initial = euler_from_quaternion(*self.current_pose.orientation())[2]
-        #align x
+        x_initial, y_initial = self.current_pose.position.x, self.current_pose.position.y
+        yaw_initial = euler_from_quaternion((self.current_pose.orientation.x, self.current_pose.orientation.y,
+                                            self.current_pose.orientation.z, self.current_pose.orientation.w))[2]
+        # align x
+        rp.loginfo(f"initial: {x_initial},{y_initial},{yaw_initial}")
+        rp.loginfo(f"goal: {x_goal},{y_goal},{yaw_goal}")
         ang = (0 if x_goal - x_initial >= 0 else math.pi)
         dir_ang = (-1 if yaw_initial - ang > 0 else 1)
-        instructions.append((0, self.v_ang * dir_ang, abs(ang - yaw_initial) / self.v_ang))
-        instructions.append((self.v, 0, abs(x_initial - x_goal) / self.v))
+        if yaw_initial-ang != 0:
+            instructions.append((0, self.v_ang * dir_ang, abs(ang - yaw_initial) / self.v_ang))
+            rp.loginfo(f"""se alineo angular x""")
+
+        if x_initial-x_goal !=0:
+            instructions.append((self.v, 0, abs(x_initial - x_goal) / self.v))
+            rp.loginfo(f"""se movio linear x""")
+
 
         # align y
         yaw_initial = ang
         ang = (math.pi/2 if y_goal - y_initial > 0 else -math.pi/2)
-        dir_ang = (-1 if yaw_initial - ang > 0 else 1) #sign?
-        
-        instructions.append((0, self.v_ang * dir_ang, abs(ang - yaw_initial) / self.v_ang))
-        instructions.append((self.v, 0, abs(y_initial - y_goal) / self.v))
-        
-        #yaw align
+        dir_ang = (-1 if yaw_initial - ang > 0 else 1)  # sign?
+        if yaw_initial-ang != 0:
+            rp.loginfo(f"""se alineo angular y""")
+            instructions.append((0, self.v_ang * dir_ang, abs(ang - yaw_initial) / self.v_ang))
+        if y_initial-y_goal != 0:
+            instructions.append((self.v, 0, abs(y_initial - y_goal) / self.v))
+            rp.loginfo(f"""se movio linear y""")
+
+        # yaw align
         yaw_initial = ang
-        dir_ang = (-1 if yaw_goal - yaw_initial > 0 else 1) #sign?
-        instructions.append((0, self.v_ang * dir_ang, abs(yaw_goal - yaw_initial) / self.v_ang))
-        
+        dir_ang = (-1 if yaw_initial - yaw_goal > 0 else 1)  # sign?
+        if yaw_initial-yaw_goal != 0:
+            rp.loginfo(f"""se alineo angular""")
+            instructions.append((0, self.v_ang * dir_ang, abs(yaw_goal - yaw_initial) / self.v_ang))
+
         self.aplicar_velocidad(instructions)
         self.current_pose = goal_pose
-
+        
     def accion_mover(self, pose_array: PoseArray):
         for pose in pose_array.poses:
             self.mover_robot_a_destino(pose)
+            time.sleep(1)
 
-rp.spin()
+if __name__ == "__main__":
+    m = Movement()
+    rp.spin()
--- src/lab1/scripts/pose_loader.py
+++ src/lab1/scripts/pose_loader.py
@@ -12,24 +12,23 @@ import math
 def pose_loader():
     rospy.init_node( 'pose_loader' )
     pub = rospy.Publisher( 'goal_list', PoseArray, queue_size=10 )
-    with open(path.join("src","lab1","scripts","pose_list.txt"), "r") as pl:
+    with open("/home/pato/robotica-movil-2023/src/lab1/scripts/pose_list.txt", "r") as pl:
         pose_list = [line.strip()[1:-1].split(",") for line in pl]
         parray = PoseArray()
         for p in pose_list:
             pose = Pose()
-            if len(p[2])>2:
-                y = math.pi/int(p[2][3:])
-            else:
-                y = math.pi
-
+            if p[2].isdigit():
+                y = float(p[2])%(2*math.pi)
             q = quaternion_from_euler(0, 0, y)
             q =Quaternion(*q)
             pose.orientation = q
             pose.position = Point(int(p[0]),int(p[1]),0)
             parray.poses.append(pose)
-    print(parray)
+    while not rospy.is_shutdown() and pub.get_num_connections() == 0:
+        pass
     pub.publish(parray)
+    rospy.loginfo(parray)
 
-if __name__ == 'main':
+if __name__ == '__main__':
     pose_loader()
     rospy.spin()
