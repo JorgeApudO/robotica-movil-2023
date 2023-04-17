cdc0303353a728f29535826128b095ad1b08f022
Martin Atria Paublo <maratripa@gmail.com>
2023-04-17 14:48:57 -0400

    PEP-8

--- src/lab1/scripts/dead_reckoning_nav_2.py
+++ src/lab1/scripts/dead_reckoning_nav_2.py
@@ -13,13 +13,17 @@ class Movement(object):
         self.v = 0.2  # m/s
         self.v_ang = 1  # rads/s
         rp.init_node("Movement_manager")
-        self.vel_applier = rp.Publisher("yocs_cmd_vel_mux/input/navigation", Twist, queue_size=10)
+        self.vel_applier = rp.Publisher("yocs_cmd_vel_mux/input/navigation",
+                                        Twist, queue_size=10)
         self.rate_hz = 10
         self.rate = rp.Rate(self.rate_hz)
         self.mov_sub = rp.Subscriber("goal_list", PoseArray,
                                      self.accion_mover)  # investigar PoseArray
         self.current_pose = Pose()
-        self.obs_sub = rp.Subscriber('/occupancy_state', Vector3, self.update_obstacles)
+        self.obs_sub = rp.Subscriber('/occupancy_state', Vector3,
+                                     self.update_obstacles)
+        self.sound_pub = rp.Publisher('sound_play/SoundRequest', SoundRequest,
+                                      queue_size=2)
         self.obs = (0, 0, 0)
         self.muro = ''
         self.movimiento = True
@@ -43,6 +47,7 @@ class Movement(object):
                     if not self.ruido:
                         # Hace ruido
                         sound = SoundRequest(-3, 1, 1.0, f"Muro {self.muro}")
+                        self.sound_pub.publish(sound)
                         self.ruido = False
                     speed = Twist()
                     self.vel_applier.publish(speed)
@@ -57,13 +62,18 @@ class Movement(object):
 
     def mover_robot_a_destino(self, goal_pose: Pose):
         x_goal, y_goal = goal_pose.position.x, goal_pose.position.y
-        yaw_goal = euler_from_quaternion((goal_pose.orientation.x, goal_pose.orientation.y,
-                                         goal_pose.orientation.z, goal_pose.orientation.w))[2]
+        yaw_goal = euler_from_quaternion((goal_pose.orientation.x,
+                                          goal_pose.orientation.y,
+                                          goal_pose.orientation.z,
+                                          goal_pose.orientation.w))[2]
         instructions = []
-        x_initial, y_initial = self.current_pose.position.x, self.current_pose.position.y
-        yaw_initial = euler_from_quaternion((self.current_pose.orientation.x, self.current_pose.orientation.y,
-                                            self.current_pose.orientation.z, self.current_pose.orientation.w))[2]
-        yaw_initial = self.corregir_angulo(yaw_initial)
+        x_initial, y_initial = self.current_pose.position.x, \
+            self.current_pose.position.y
+        yaw_initial = euler_from_quaternion((self.current_pose.orientation.x,
+                                             self.current_pose.orientation.y,
+                                             self.current_pose.orientation.z,
+                                             self.current_pose.orientation.w))
+        yaw_initial = self.corregir_angulo(yaw_initial[2])
 
         # align x
         if x_initial-x_goal != 0:
@@ -71,7 +81,8 @@ class Movement(object):
             ang_aplicado = self.corregir_angulo(yaw_initial - ang)
             dir_ang = -1 if ang_aplicado > 0 else 1
             if yaw_initial-ang != 0:
-                instructions.append((0, self.v_ang * dir_ang, self.factor * abs(ang_aplicado) / self.v_ang))
+                instructions.append((0, self.v_ang * dir_ang,
+                                     self.factor * abs(ang_aplicado) / self.v_ang))
             instructions.append((self.v, 0, abs(x_initial - x_goal) / self.v))
             yaw_initial = self.corregir_angulo(ang)
 
@@ -82,7 +93,8 @@ class Movement(object):
             ang_aplicado = self.corregir_angulo(yaw_initial - ang)
             dir_ang = -1 if ang_aplicado > 0 else 1
             if yaw_initial-ang != 0:
-                instructions.append((0, self.v_ang * dir_ang, self.factor * abs(ang_aplicado) / self.v_ang))
+                instructions.append((0, self.v_ang * dir_ang,
+                                     self.factor * abs(ang_aplicado) / self.v_ang))
             instructions.append((self.v, 0, abs(y_initial - y_goal) / self.v))
             yaw_initial = self.corregir_angulo(ang)
 
@@ -90,7 +102,8 @@ class Movement(object):
         if yaw_initial-yaw_goal != 0:
             ang_aplicado = self.corregir_angulo(yaw_initial - yaw_goal)
             dir_ang = -1 if ang_aplicado > 0 else 1
-            instructions.append((0, self.v_ang * dir_ang, self.factor * abs(ang_aplicado) / self.v_ang))
+            instructions.append((0, self.v_ang * dir_ang,
+                                 self.factor * abs(ang_aplicado) / self.v_ang))
 
         self.aplicar_velocidad(instructions)
         self.current_pose = goal_pose
