bd07f268a98e5b56830caca0759f61b2a3f317d8
Martin Atria Paublo <maratripa@gmail.com>
2023-05-16 23:53:43 -0400

    base code

---
+++ src/lab2/scripts/follow_square.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+import rospy
+import numpy as np
+
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from std_msgs.msg import Float64
+
+
+class Robot():
+    def __init__(self):
+        rospy.init_node("follow_square_robot")
+
+        # --------------------------------------------------------------------
+        # INITIAL CONDITIONS
+        # --------------------------------------------------------------------
+        self.ang = 0.0
+        self.goal_ang = 0.0
+
+        self.vel = 0.0
+        self.ang_vel = 0.0
+
+        # --------------------------------------------------------------------
+        # VEL PUBLISHER NODE
+        # --------------------------------------------------------------------
+        self.vel_applier = rospy.Publisher("yocs_cmd_vel_mux/input/navigation",
+                                           Twist, queue_size=1)
+
+        # --------------------------------------------------------------------
+        # POSE NODE
+        # --------------------------------------------------------------------
+        self.odom_sub = rospy.Subscriber('odom', Odometry,
+                                         self.odom_fn)
+
+        # --------------------------------------------------------------------
+        # ANGLE PID CONTROL
+        # --------------------------------------------------------------------
+        self.ang_set_point = rospy.Publisher('/blue_square/setpoint',
+                                             Float64, queue_size=1)
+        while self.ang_set_point.get_num_connections() == 0 and not rospy.is_shutdown():
+            rospy.sleep(0.1)
+
+        self.ang_state = rospy.Publisher('/blue_square/state',
+                                         Float64, queue_size=1)
+        while self.ang_state.get_num_connections() == 0 and not rospy.is_shutdown():
+            rospy.sleep(0.1)
+
+        self.ang_actuation = rospy.Subscriber('/blue_square/control_effect',
+                                              Float64, self.ang_actuation_fn)
+
+        # --------------------------------------------------------------------
+        # TIMER
+        # --------------------------------------------------------------------
+        self.period = 0.1
+        rospy.Timer(rospy.Duration(self.period), self.publish_odom)
+
+    def ang_actuation_fn(self, data):
+        self.ang_vel = float(data.data)
+        self.publish_vel()
+
+    def odom_fn(self, data):
+        pass
+
+    def publish_odom(self, _):
+        self.ang_state.publish(0)
+
+    def publish_vel(self):
+        vel = Twist()
+        vel.linear.x = self.vel
+        vel.angular.z = self.ang_vel
+        self.vel_applier.publish(vel)
