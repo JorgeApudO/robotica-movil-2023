3edf8cfa89fb2fc4a5a8c98f89baf79d9d8eda95
JorgeApudO <japud@uc.cl>
2023-05-18 16:26:27 -0400

    file updates from lab pc

--- src/lab2/scripts/reactive_movement.py
+++ src/lab2/scripts/reactive_movement.py
@@ -6,6 +6,9 @@ from sensor_msgs.msg import Image
 from tf.transformations import euler_from_quaternion
 import numpy as np
 from cv_bridge import CvBridge
+import cv2
+
+
 
 def sawtooth(rad):
     return (rad - np.pi) % (2*np.pi) - np.pi
@@ -24,6 +27,7 @@ class Robot():
     def __init__(self):
         rospy.init_node("reckoning_robot")
         self.bridge = CvBridge()
+        self.img_guardadas=0
 
         # --------------------------------------------------------------------
         # CONSTANTS
@@ -38,7 +42,7 @@ class Robot():
         self.distance = np.array((0.0, 0.0))
         self.ang = 0.0
 
-        self.vel = 0.2
+        self.vel = 0.0
         self.ang_vel = 0.0
 
         self.rotating = False
@@ -134,6 +138,9 @@ class Robot():
 
 
         depth_image = self.bridge.imgmsg_to_cv2(data)
+
+        if self.img_guardadas<10:
+            cv2.imwrite("imagen_prof.png", depth_image)
 
         #Left depth from image
         depth_left = depth_image[100:300,100:150]
