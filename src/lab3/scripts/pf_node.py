16abc07e6e23859cc42f372999e1c99c1b5937d1
Jorge Apud <japud@uc.cl>
2023-06-29 04:02:29 -0400

    bug fixing 35

--- src/lab3/scripts/pf_node.py
+++ src/lab3/scripts/pf_node.py
@@ -200,7 +200,7 @@ class PFMap:
                     prob_pos = (1/(np.sqrt(2*np.pi)*SENSOR_DISPERSION)) * np.exp( - (min_dist_occupied ** 2) / (2 * SENSOR_DISPERSION**2))
                     prob_array.append(*prob_pos)
             q = sum(prob_array) / len(prob_pos)
-            q_array.append(q)
+            q_array.append(*q)
         q_array = np.array(q_array)
         q_array /= np.sum(q_array)
         rp.loginfo(f"{q_array}")
