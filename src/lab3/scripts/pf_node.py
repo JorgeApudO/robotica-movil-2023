38790f05b8f202bda3c56e58ce910752bd4339ea
Jorge Apud <japud@uc.cl>
2023-06-29 03:35:06 -0400

    bug fixing 21

--- src/lab3/scripts/pf_node.py
+++ src/lab3/scripts/pf_node.py
@@ -179,7 +179,7 @@ class PFMap:
         # rp.loginfo(f"{self.weights.shape}")
         rp.loginfo(f"weights: {self.weights}")
         new_particles_idx = np.random.choice(
-            self.particles.shape[0], MAX_PARTICLES, p=self.weights.T)
+            self.particles.shape[0], MAX_PARTICLES, p=self.weights)
 
         self.particles = self.particles[new_particles_idx]
 
