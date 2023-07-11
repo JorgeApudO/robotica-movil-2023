c338e48b597d56ec7745d02c2d5d81a7a25c5da7
Martin Atria Paublo <maratripa@gmail.com>
2023-07-11 16:01:58 -0400

    pep8

--- src/proyecto_grupo_2_2023/scripts/nav_stack.py
+++ src/proyecto_grupo_2_2023/scripts/nav_stack.py
@@ -24,7 +24,7 @@ class NavStack:
             f"({str(status)})")
 
     def set_goals(self, list_x, list_y, list_a):
-        for x,y,a in zip(list_x, list_y, list_a):
+        for x, y, a in zip(list_x, list_y, list_a):
             goal_pose = Pose()
             goal_pose.position.x = x
             goal_pose.position.y = y
@@ -35,11 +35,12 @@ class NavStack:
             move_base_goal.target_pose.header.frame_id = 'map'
             move_base_goal.target_pose.header.stamp = rp.Time.now()
             move_base_goal.target_pose.pose = goal_pose
-            self.move_base_client.send_goal(move_base_goal, done_cb=self.goal_done)
+            self.move_base_client.send_goal(
+                move_base_goal, done_cb=self.goal_done)
             self.move_base_client.wait_for_result()
 
 
 if __name__ == "__main__":
     nav_stack = NavStack()
-    nav_stack.set_goals((1,1,3), (1,2,1), (0, np.pi, np.pi/2))
+    nav_stack.set_goals((1, 1, 3), (1, 2, 1), (0, np.pi, np.pi/2))
     rp.spin()
