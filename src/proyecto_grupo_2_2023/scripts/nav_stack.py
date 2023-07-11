69093f42b20d530181a95d46df6a9667677160ef
Dvckhv <patricio.hinostrozav@uc.cl>
2023-07-11 16:00:00 -0400

    nav_stack_done

--- src/proyecto_grupo_2_2023/scripts/nav_stack.py
+++ src/proyecto_grupo_2_2023/scripts/nav_stack.py
@@ -2,6 +2,7 @@
 
 import rospy as rp
 import actionlib
+import numpy as np
 
 from tf.transformations import quaternion_from_euler
 
@@ -22,21 +23,23 @@ class NavStack:
             f"DONE status: {self.move_base_client.get_goal_status_text()} " +
             f"({str(status)})")
 
-    def set_goal(self, x, y, a):
-        goal_pose = Pose()
-        goal_pose.position.x = x
-        goal_pose.position.y = y
-        q = quaternion_from_euler(0, 0, a)
-        goal_pose.orientation = Quaternion(*q)
+    def set_goals(self, list_x, list_y, list_a):
+        for x,y,a in zip(list_x, list_y, list_a):
+            goal_pose = Pose()
+            goal_pose.position.x = x
+            goal_pose.position.y = y
+            q = quaternion_from_euler(0, 0, a)
+            goal_pose.orientation = Quaternion(*q)
 
-        move_base_goal = MoveBaseGoal()
-        move_base_goal.target_pose.header.frame_id = 'map'
-        move_base_goal.target_pose.header.stamp = rp.Time.now()
-        move_base_goal.target_pose.pose = goal_pose
-        self.move_base_client.send_goal(move_base_goal, done_cb=self.goal_done)
+            move_base_goal = MoveBaseGoal()
+            move_base_goal.target_pose.header.frame_id = 'map'
+            move_base_goal.target_pose.header.stamp = rp.Time.now()
+            move_base_goal.target_pose.pose = goal_pose
+            self.move_base_client.send_goal(move_base_goal, done_cb=self.goal_done)
+            self.move_base_client.wait_for_result()
 
 
 if __name__ == "__main__":
     nav_stack = NavStack()
-
+    nav_stack.set_goals((1,1,3), (1,2,1), (0, np.pi, np.pi/2))
     rp.spin()
