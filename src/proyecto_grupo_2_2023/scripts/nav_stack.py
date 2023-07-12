abf2876e54df1420d160307452a44394351bf923
Martin Atria Paublo <maratripa@gmail.com>
2023-07-12 00:52:50 -0400

    refactor stack

--- src/proyecto_grupo_2_2023/scripts/nav_stack.py
+++ src/proyecto_grupo_2_2023/scripts/nav_stack.py
@@ -10,73 +10,83 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from geometry_msgs.msg import Pose, Quaternion, PoseWithCovarianceStamped
 
 
+def get_move_base_goal(pose):
+    move_base_goal = MoveBaseGoal()
+    move_base_goal.target_pose.header.frame_id = 'map'
+    move_base_goal.target_pose.header.stamp = rp.Time.now()
+    move_base_goal.target_pose.pose = pose
+    return move_base_goal
+
+
 class NavStack:
     def __init__(self):
         rp.init_node('nav_stack')
 
         self.wait = True
-        rp.Subscriber("amcl_pose", PoseWithCovarianceStamped, self.init_pos_setter ,queue_size=1)
+        rp.Subscriber("amcl_pose", PoseWithCovarianceStamped,
+                      self.pose_update, queue_size=1)
 
         self.move_base_client = actionlib.SimpleActionClient(
             '/move_base', MoveBaseAction)
         self.move_base_client.wait_for_server()
 
-    def init_pos_setter(self, data):
-        if self.wait:
-            rp.loginfo(f"inital pose setting")
-            initial_pose = data.pose.pose
+        self.position_cov = PoseWithCovarianceStamped()
 
-            self.initial_pos = Pose()
+        self.pose_queue = []
 
-            self.initial_pos.position.x = initial_pose.position.x
-            self.initial_pos.position.y = initial_pose.position.y
-            self.initial_pos.orientation = initial_pose.orientation
-
-            self.wait = False
-        
+    def pose_update(self, data):
+        self.position_cov = data
 
     def goal_done(self, status, result):
         rp.loginfo(
             f"DONE status: {self.move_base_client.get_goal_status_text()} " +
             f"({str(status)})")
 
-    def set_goals(self, list_x, list_y, list_a):
+    def add_goal(self, x, y, a):
+        goal = Pose()
+        goal.position.x = x
+        goal.position.y = y
+        goal.orientation = Quaternion(*quaternion_from_euler(0, 0, a))
 
-        for i in range(2):
-            ang = euler_from_quaternion((self.initial_pos.orientation.x,
-                                        self.initial_pos.orientation.y,
-                                        self.initial_pos.orientation.w,
-                                        self.initial_pos.orientation.z
-                                        ))[2] + i*(np.pi)/2
-            list_x.insert(0, self.initial_pos.position.x)
-            list_y.insert(0, self.initial_pos.position.y)
-            list_a.insert(0, ang)
-        print(list_x)
-        print(list_y)
-        print(list_a)
-        for x, y, a in zip(list_x, list_y, list_a):
-            goal_pose = Pose()
-            goal_pose.position.x = x
-            goal_pose.position.y = y
-            q = quaternion_from_euler(0, 0, a)
-            goal_pose.orientation = Quaternion(*q)
+        self.pose_queue.append(goal)
 
-            move_base_goal = MoveBaseGoal()
-            move_base_goal.target_pose.header.frame_id = 'map'
-            move_base_goal.target_pose.header.stamp = rp.Time.now()
-            move_base_goal.target_pose.pose = goal_pose
-            self.move_base_client.send_goal(
-                move_base_goal, done_cb=self.goal_done)
+    def rotate(self):
+        first = True
+
+        while first or cov_x > 1 or cov_y > 1:
+            ang = euler_from_quaternion(
+                (self.position_cov.pose.pose.orientation.x,
+                 self.position_cov.pose.pose.orientation.y,
+                 self.position_cov.pose.pose.orientation.w,
+                 self.position_cov.pose.pose.orientation.z))[2]
+            ang += np.pi/18  # 5 grados
+            move_base_goal = get_move_base_goal(self.position_cov.pose.pose)
+            move_base_goal.target_pose.pose.orientation = Quaternion(
+                *quaternion_from_euler(0, 0, ang))
+
+            self.move_base_client.send_goal(move_base_goal)
+            if first:
+                self.move_base_client.wait_for_result(rp.Duration(15))
+                first = False
+            else:
+                self.move_base_client.wait_for_result(rp.Duration(5))
+
+            cov_x = self.position_cov.pose.covariance[0]
+            cov_y = self.position_cov.pose.covariance[7]
+            rp.loginfo(f"cov x: {cov_x}, cov y: {cov_y}")
+
+    def main_loop(self):
+        while not rp.is_shutdown() and self.pose_queue:
+            goal = get_move_base_goal(self.pose_queue.pop(0))
+            self.move_base_client.send_goal(goal, done_cb=self.goal_done)
             self.move_base_client.wait_for_result()
-            rp.loginfo(f"Arrived at: (x:{x}, y:{y}, yaw:{a})")
 
 
 if __name__ == "__main__":
     nav_stack = NavStack()
 
-    rp.loginfo("Waiting for initial pose")
-    while nav_stack.wait:
-        rp.sleep(0.1)
-    
-    nav_stack.set_goals([1, 1, 3], [1, 2, 1], [0, np.pi, np.pi/2])
-    rp.spin()
+    nav_stack.rotate()
+    nav_stack.add_goal(25, 1, 0)
+    nav_stack.add_goal(27, 12, np.pi/2)
+    nav_stack.add_goal(7, 15, 3*np.pi/4)
+    nav_stack.main_loop()
